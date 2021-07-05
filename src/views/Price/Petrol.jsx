444abcdad8e76a23c57f51731bfdee229966f7b4
Bucky <bucky0112@gmail.com>
2021-07-05 13:19:10 +0800

    1.install axios 2.petrol page ok

--- src/views/Price/Petrol.jsx
+++ src/views/Price/Petrol.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Card, Row, Col } from 'antd'
 
 const Petrol = () => {
-  return <div>Petrol</div>
+  const [petrolData] = useState([
+    {
+      title: '無鉛汽油92 --- 價格走勢圖',
+      link: 'http://just2.entrust.com.tw/z/ze/zeq/zeqa_D0131320.djhtm'
+    },
+    {
+      title: '無鉛汽油95 --- 價格走勢圖',
+      link: 'http://just2.entrust.com.tw/z/ze/zeq/zeqa_D0131330.djhtm'
+    },
+    {
+      title: '無鉛汽油98 --- 價格走勢圖',
+      link: 'http://just2.entrust.com.tw/z/ze/zeq/zeqa_D0131340.djhtm'
+    }
+  ])
+
+  return (
+    <>
+      <Row>
+        {petrolData.map((item) => {
+          return (
+            <Col lg={12} md={24} sm={24} xs={24} key={item.title}>
+              <Card style={{ margin: 10 }}>
+                <div
+                  style={{
+                    height: '45vh',
+                    overflow: 'hidden',
+                    position: 'relative'
+                  }}
+                >
+                  <iframe
+                    src={item.link}
+                    height='100%'
+                    width='100%'
+                    frameBorder='0'
+                    style={{
+                      position: 'absolute',
+                      left: '-10px'
+                    }}
+                  />
+                </div>
+              </Card>
+            </Col>
+          )
+        })}
+      </Row>
+    </>
+  )
 }
 
 export default Petrol
